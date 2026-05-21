7eb8f693d6475c7d4a44054c0ea14bf4f2fa98d7
Antonio <antonio.valenti.213@gmail.com>
2026-05-21 19:08:52 +0200

    pusha tutto

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,28 @@
 
+import "./Components.css"
+
+import { FaCalendarDay } from "react-icons/fa";
+import { FaTicket } from "react-icons/fa6";
 
 export default function  NavBar() {
     return(
-        <h1>Babbo</h1>
+        <div className="NavBarMain">
+
+            <img src={}/>
+
+            <div className="InfoButtons">
+                <button className="InfoButton">
+                    <FaCalendarDay/>
+                    Calendario
+                </button>
+                <button className="InfoButton">
+                    <FaTicket/>
+                    Prenota adesso
+                </button>
+            </div>
+
+        </div>
+
     )
 }
 
